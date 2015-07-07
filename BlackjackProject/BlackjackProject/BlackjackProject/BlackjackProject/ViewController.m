1c70f89b87659965fb9af79709bc2f09dff6c400
ArthurTorrente <torrente.arthur@gmail.com>
2015-07-07 23:32:27 +0200

    no message

---
+++ BlackjackProject/BlackjackProject/BlackjackProject/BlackjackProject/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  BlackjackProject
+//
+//  Created by arivet on 16/02/2015.
+//  Copyright (c) 2015 bhrtw. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GameManager.h"
+#import "Hand.h"
+#import "Card.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _gm = [[GameManager alloc] initWithNibName:@"GameManager" bundle:nil];
+    [self.view addSubview:_gm.view];
+    
+    self.navigationController.navigationBar.hidden = YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
